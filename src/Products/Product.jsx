eedbb0541fd40b88699ad7d7cdbe9db60eb023a0
Rizwanishere <rizwantopper@gmail.com>
2024-05-31 11:34:43 +0530

    Added to cart toast

--- src/Products/Product.jsx
+++ src/Products/Product.jsx
@@ -2,13 +2,21 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
 import { CartContext } from "../Contexts/CartContext";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Actions({ product }) {
   const { addToCart } = useContext(CartContext);
   const { id } = product;
+
+  const handleAddToCart = () => {
+    addToCart(product, id);
+    toast.success('Added to cart!');
+  };
+
   return (
     <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300 hidden md:flex">
-      <button onClick={() => addToCart(product, id)}>
+      <button onClick={handleAddToCart}>
         <div className="flex justify-center items-center text-white w-12 h-12 bg-primary">
           <BsPlus className="text-3xl" />
         </div>
@@ -26,6 +34,11 @@ function Actions({ product }) {
 function Product({ product }) {
   const { addToCart } = useContext(CartContext);
   const { id, image, category, title, price } = product;
+
+  const handleAddToCart = () => {
+    addToCart(product, id);
+    toast.success('Added to cart!');
+  };
   return (
     <div className="border border-[#e4e4e4] h-[400px] mb-4 relative overflow-hidden group transition rounded-lg hover:border-primary shadow-lg">
       <div className="w-full h-full flex flex-col justify-between">
@@ -46,7 +59,7 @@ function Product({ product }) {
           <div className="font-semibold">$ {price}</div>
           <div className="flex justify-center items-center mt-4 md:hidden space-x-4">
             <button
-              onClick={() => addToCart(product, id)}
+              onClick={handleAddToCart}
               className="text-white bg-primary hover:bg-secondary rounded px-4 py-2 hover:bg-white hover:text-primary hover:border hover:border-primary"
             >
               Buy Now
